db15e4afda0596a14cf45cc96c00a6e7381a6deb
Narlotl <fretwellcalifornia@gmail.com>
2023-12-21 11:41:47 -0800

    Force limit to reduce data sent

--- README.md
+++ README.md
@@ -5,19 +5,76 @@ An API for [NGS geodetic survey markers](https://geodesy.noaa.gov/datasheets/).
 ## Endpoints
 
 Get markers: `https://us-central1-survey-markers.cloudfunctions.net/getMarkers?parameters`\
-Get images: `https://us-central1-survey-markers.cloudfunctions.net/getImages?id=ID`
+Example response: 
+```javascript
+{
+    "markers": [
+        {
+            // Type of marker
+            "marker": "CHISELED TRIANGLE",
+            // What it's in
+            "setting": "SET IN A MASSIVE STRUCTURE",
+            // Specific setting (uncommon)
+            "spSet": "BUILDING",
+            // Stability
+            "stability": "PROBABLY HOLD POSITION/ELEVATION WELL",
+            // Marker description
+            "description": "BY COAST AND GEODETIC SURVEY 1956 AT SAN FRANCISCO. AT SAN FRANCISCO, ALONG SEVENTH STREET, BETWEEN KING AND TOWNSEND STREETS, AT A THREE STORY BRICK BUILDING OCCUPIED BY BAKER AND HAMILTON, ON TOP OF THE NORTHWEST END OF THE BOTTOM STEP OF THE MAIN ENTRANCE, 125 FEET SOUTHWEST OF THE CENTER LINE OF TOWNSEND STREET, 0.5 FOOT SOUTHEAST OF THE SOUTHEAST END OF THE BRICK WALL AT THE NORTHEAST DOOR, 0.7 FOOT HIGHER THAN THE SIDEWALK, AND ABOUT 1 FOOT HIGHER THAN THE STREET.",
+            // Height above sea level (meters)
+            "elevation": 4.54,
+            // Marker report history
+            "history": [
+                {
+                    "date": "UNK",
+                    "condition": "MONUMENTED",
+                    "reporter": "CA3290"
+                },
+                {
+                    "date": "1956",
+                    "condition": "GOOD",
+                    "reporter": "CGS"
+                }
+            ],
+            // Marker PID
+            "id": "HT0754",
+            // Location
+            "latitude": 37.7725,
+            "longitude": -122.4025
+        }
+    ],
+    // Whether a limit was forced
+    "paginated": false,
+    // How many markers were searched
+    "endIndex": 1,
+    // How many markers in the state
+    "total": 62234
+}
+```
+Get images: `https://us-central1-survey-markers.cloudfunctions.net/getImages?id=ID`\
+Example response:
+```javascript
+// List of images for the marker
+// Empty if no images
+[
+    "https://geodesy.noaa.gov/cgi-bin/get_image.prl?PROCESSING=get_image&PID=HV1846&filename=HV1846-MERIDIAN_STONE-1-20131105.jpg",
+    "https://geodesy.noaa.gov/cgi-bin/get_image.prl?PROCESSING=get_image&PID=HV1846&filename=HV1846-MERIDIAN_STONE-2-20190321.jpg",
+    "https://geodesy.noaa.gov/cgi-bin/get_image.prl?PROCESSING=get_image&PID=HV1846&filename=HV1846-MERIDIAN_STONE-3N-20131025.jpg",
+    "https://geodesy.noaa.gov/cgi-bin/get_image.prl?PROCESSING=get_image&PID=HV1846&filename=HV1846-MERIDIAN_STONE-3S-20131107.jpg",
+    "https://geodesy.noaa.gov/cgi-bin/get_image.prl?PROCESSING=get_image&PID=HV1846&filename=HV1846-MERIDIAN_STONE-3E-20120302.jpg",
+    "https://geodesy.noaa.gov/cgi-bin/get_image.prl?PROCESSING=get_image&PID=HV1846&filename=HV1846-MERIDIAN_STONE-3W-20120302.jpg",
+    "https://geodesy.noaa.gov/cgi-bin/get_image.prl?PROCESSING=get_image&PID=HV1846&filename=HV1846-MERIDIAN_STONE-1-20201212.jpg",
+    "https://geodesy.noaa.gov/cgi-bin/get_image.prl?PROCESSING=get_image&PID=HV1846&filename=HV1846-MERIDIAN_STONE-2-20201212.jpg"
+]
+```
 
 ### Parameters
-
-> [!WARNING]
-> Parameter filtering is only available if the state's JSON file is less than 32MB. Requests to states over 32MB will redirect to the state's direct file and won't be filtered.
-> States over 32MB: FL, CA, TX, NC, MN, SC, WA.
-> Example: `https://us-central1-survey-markers.cloudfunctions.net/getMarkers?state=ca` → `https://firebasestorage.googleapis.com/v0/b/survey-markers.appspot.com/o/ca.json`
 
 | Name | Description | Required |
 | ---- | ----------- | -------- |
 | state | The state/territory to retrieve in 2 letter code format. State codes can be found at `https://firebasestorage.googleapis.com/v0/b/survey-markers.appspot.com/o/states.json?alt=media&token=56e7422d-1377-4aa4-8607-30d946323b09`. | `true` |
 | data | The data fields to return (will return all data if left empty) | recommended, especially for large states |
+| offset | How many markers to skip | `false` |
+| limit | How many markers to return (a limit is forced on large requests) | `false` |
 | id | The single marker ID to return | `false` |
 | location | The lat/long coordinates to center the search | with radius |
 | radius | How far in kilometers to search from `location` | with location |
--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,58 @@
 const { initializeApp } = require('firebase/app');
 const { onRequest } = require('firebase-functions/v2/https');
-const { getStorage, ref, connectStorageEmulator, getDownloadURL, listAll, getMetadata } = require('firebase/storage');
+const { getStorage, ref, connectStorageEmulator, getDownloadURL, listAll, getMetadata, getStream } = require('firebase/storage');
 const { firebaseConfig } = require('./secrets');
+
+const MAX_BYTES = 31.5 * 2 ** 20;
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 // Only for testing
 // connectStorageEmulator(storage, '127.0.0.1', 9199);
 
+// Haversine formula 
+// https://community.esri.com/t5/coordinate-reference-systems-blog/distance-on-a-sphere-the-haversine-formula/ba-p/902128#:~:text=Calculate distance using the Haversine Formula
 const haversine = (lat1, long1, lat2, long2) => {
-    // Haversine formula 
-    // https://community.esri.com/t5/coordinate-reference-systems-blog/distance-on-a-sphere-the-haversine-formula/ba-p/902128#:~:text=Calculate distance using the Haversine Formula
     const a = Math.sin((lat2 - lat1) * Math.PI / 180 / 2) ** 2 +
         Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
         Math.sin((long2 - long1) * Math.PI / 180 / 2) ** 2;
     return (6371 * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)));
 };
+
+// https://stackoverflow.com/a/11900218
+const roughSizeOfObject = object => {
+    const objectList = [];
+    const stack = [object];
+    let bytes = 0;
+
+    while (stack.length) {
+        const value = stack.pop();
+
+        switch (typeof value) {
+            case 'boolean':
+                bytes += 4;
+                break;
+            case 'string':
+                bytes += value.length * 2;
+                break;
+            case 'number':
+                bytes += 8;
+                break;
+            case 'object':
+                if (!objectList.includes(value)) {
+                    objectList.push(value);
+                    for (const prop in value) {
+                        if (value.hasOwnProperty(prop)) {
+                            stack.push(value[prop]);
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    return bytes;
+}
 
 exports.getMarkers = onRequest((req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
@@ -24,76 +61,75 @@ exports.getMarkers = onRequest((req, res) => {
         const state = req.query.state.toLowerCase();
         // Download data
         const file = ref(storage, state + '.json');
-        getMetadata(file).then(metadata => {
-            const downloadUrl = getDownloadURL(file);
-            if (metadata.size >= 33554432)
-                downloadUrl.then(url => res.redirect(url));
+        let data = '';
+        getDownloadURL(file).then(url => fetch(url)).then(res => res.json()).then(data => {
+            // Filter by parameters
+            let ret = [], bytes = 0;
+            let dataParam, location, offset, limit, reporter;
+            if (req.query.data)
+                dataParam = req.query.data.split(',');
+            if (req.query.location)
+                location = req.query.location.split(',');
+            if (req.query.offset)
+                offset = parseInt(req.query.offset);
             else
-                downloadUrl.then(url => fetch(url)).then(res => res.json()).then(data => {
-                    // Filter by parameters
-                    let ret = [];
-                    let dataParam, location, offset, limit, reporter;
-                    if (req.query.data)
-                        dataParam = req.query.data.split(',');
-                    if (req.query.location)
-                        location = req.query.location.split(',');
-                    if (req.query.offset)
-                        offset = parseInt(req.query.offset);
-                    else
-                        offset = 0;
-                    if (req.query.limit)
-                        limit = Math.min(offset + parseInt(req.query.limit, data.length));
-                    else
-                        limit = data.length;
-                    if (req.query.reporter)
-                        reporter = req.query.reporter.toUpperCase().split(',');
-                    for (let i = offset; i < limit; i++) {
-                        const marker = data[i];
-                        let obj = {};
-                        // Filter by ID
-                        if (req.query.id)
-                            if (marker.id != req.query.id)
-                                continue;
-                        // Filter by search
-                        if (req.query.search)
-                            if (!marker.description.match(new RegExp(req.query.search, 'gmi')))
-                                continue;
-                        // Filter by condition
-                        if (req.query.condition)
-                            if (!marker.history || marker.history.length == 0 || marker.history[marker.history.length - 1].condition != req.query.condition.toUpperCase())
-                                continue;
-                        // Filter by reporter
-                        if (req.query.reporter) {
-                            let hasReporter = false;
+                offset = 0;
+            if (req.query.limit)
+                limit = Math.min(offset + parseInt(req.query.limit), data.length);
+            else
+                limit = data.length;
+            if (req.query.reporter)
+                reporter = req.query.reporter.toUpperCase().split(',');
 
-                            for (const rep of reporter)
-                                for (const report of marker.history)
-                                    if (report.reporter == rep) {
-                                        hasReporter = true;
-                                        break;
-                                    }
-                            if (!hasReporter || (!marker.history || marker.history.length == 0))
-                                continue;
-                        }
-                        // Filter by loction and radius
-                        if (location && req.query.radius)
-                            if (haversine(marker.lat, marker.long, location[0], location[1]) > req.query.radius)
-                                continue;
-                        // Only include specified data
-                        if (dataParam) {
-                            for (const datum of dataParam)
-                                if (marker[datum])
-                                    obj[datum] = marker[datum];
-                        }
-                        // Send all data
-                        else
-                            obj = marker;
+            let done = 0, i;
+            for (i = offset; done < limit && bytes < MAX_BYTES; i++) {
+                const marker = data[i];
+                let obj = {};
+                // Filter by ID
+                if (req.query.id)
+                    if (marker.id != req.query.id)
+                        continue;
+                // Filter by search
+                if (req.query.search)
+                    if (!marker.description.match(new RegExp(req.query.search, 'gmi')))
+                        continue;
+                // Filter by condition
+                if (req.query.condition)
+                    if (!marker.history || marker.history.length == 0 || marker.history[marker.history.length - 1].condition != req.query.condition.toUpperCase())
+                        continue;
+                // Filter by reporter
+                if (req.query.reporter) {
+                    let hasReporter = false;
 
-                        ret.push(obj);
-                    }
+                    for (const rep of reporter)
+                        for (const report of marker.history)
+                            if (report.reporter == rep) {
+                                hasReporter = true;
+                                break;
+                            }
+                    if (!hasReporter || (!marker.history || marker.history.length == 0))
+                        continue;
+                }
+                // Filter by loction and radius
+                if (location && req.query.radius)
+                    if (haversine(marker.latitude, marker.longitude, location[0], location[1]) > req.query.radius)
+                        continue;
+                // Only include specified data
+                if (dataParam) {
+                    for (const datum of dataParam)
+                        if (marker[datum])
+                            obj[datum] = marker[datum];
+                }
+                // Send all data
+                else
+                    obj = marker;
 
-                    res.json(ret).status(200).end();
-                });
+                done++;
+                ret.push(obj);
+                bytes += roughSizeOfObject(obj);
+            }
+
+            res.json({ markers: ret, paginated: bytes > MAX_BYTES, endIndex: i, total: data.length }).status(200).end();
         });
     }
     catch (e) {
@@ -111,7 +147,7 @@ exports.getImages = onRequest((req, res) => {
     if (req.query.id)
         fetch('https://geodesy.noaa.gov/cgi-bin/get_image.prl?PROCESSING=list&PID=' + req.query.id).then(res => res.text()).then(data => {
             if (data.includes('ERROR'))
-                res.status(422).json({ code: 422, message: 'Invalid ID format, must be 2 uppercase letters followed by 4 digits' }).end();
+                res.status(400).json({ code: 400, message: 'Invalid ID format, must be 2 uppercase letters followed by 4 digits' }).end();
             else if (data.includes('No images found'))
                 res.status(200).json([]).end();
             else
@@ -121,5 +157,5 @@ exports.getImages = onRequest((req, res) => {
                 ).end();
         });
     else
-        res.status(422).json({ code: 422, message: 'Missing ID parameter' }).end();
+        res.status(400).json({ code: 400, message: 'Missing ID parameter' }).end();
 });
